287e0c201a9c1242eab73a89e33bbad5ab3e8749
gabriele <gabrielescodinu96@gmail.com>
2021-03-03 17:38:54 +0100

    add some covers

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -1,32 +1,16 @@
 @extends('layouts.dashboard')
 
 @section('content')
+        <div class="flex-center position-ref full-height">
 
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ route('admin.index') }}">Admin</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+            <div class="content">
+                <div class="title m-b-md">
+                    <!-- <img width="200px" src="img/DC_Main.png" alt=""> -->
+                </div>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
+                <div class="links">
+                    <a href="{{ route('posts') }}">SEE ALL COMICS</a>
+                </div>
+            </div>
         </div>
-    @endif
-
-    <div class="content">
-        <div class="title m-b-md">
-            <img width="200px" src="img/DC_Main.png" alt="">
-        </div>
-
-        <div class="links">
-            <a href="{{ route('posts') }}">SEE ALL COMICS</a>
-        </div>
-    </div>
-</div>
-
 @endsection
-
--- resources/views/guests/posts/index.blade.php
+++ resources/views/guests/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Comics and Graphic Novel</h1>
+    <h1 class="novel">Comics and Graphic Novels</h1>
 
     <div class="comics">
         @foreach ($posts as $post)
